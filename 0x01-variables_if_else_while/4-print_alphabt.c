f23b93e6c8657ab85ad50366ab0b2deb384a9e0c
ZenGuri <redazengouri@gmail.com>
2023-03-19 06:41:15 -0700

    5. Numbers

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -4,11 +4,12 @@
  *  *  * main - Entry point
  *   *   * Description: 'print alphabets expect q and e'
  *    *    * Return: Always 0
- *     */
+ */
 
 int main(void)
 {
 	int n = 97;
+
 	while (n <= 122)
 	{
 		if (n == 101 || n == 113)
