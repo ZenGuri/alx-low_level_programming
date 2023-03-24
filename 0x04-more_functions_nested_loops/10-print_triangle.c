79de5f299b9ae62a011b535742327f087bdeba48
ZenGuri <redazengouri@gmail.com>
2023-03-24 17:52:38 +0100

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -13,17 +13,14 @@ void print_triangle(int size)
 int i, j;
 
 if (size <= 0)
-_putchar('\n');
+	_putchar('\n');
 for (i = 1; i <= size; i++)
 {
-for (j = 1; j <= size; j++)
-{
-if (j <= (size - i))
-_putchar(' ');
-															else
-_putchar('#');
-}
-_putchar('\n');
-}
-
+	for (j = 1; j <= size; j++)
+	{
+		if (j <= (size - i))
+			_putchar(' ');
+		else _putchar('#');
+	}
+	_putchar('\n');}
 }
