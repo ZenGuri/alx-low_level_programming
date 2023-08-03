3b224869385e1b402f66f62b2e91d408d722f32d
ZenGuri <redazengouri@gmail.com>
2023-08-03 09:58:55 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,13 +6,11 @@
  */
 void _puts_recursion(char *s)
 {
-    if (*s == '\0') // Base case: If the current character is '\0', it's the end of the string.
+    if (*s)
     {
-        _putchar('\n'); // Print a new line character.
-        return; // End the recursion.
+	    _putchar(*s);
+	    _puts_recursion(s + 1);
     }
-
-    _putchar(*s); // Print the current character.
-    _puts_recursion(s + 1); // Recursively call the function with the next character in the string.
+    else
+	    _putchar('\n');
 }
-
