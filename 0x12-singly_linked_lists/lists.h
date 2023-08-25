6be778a694623b1269632c845ce6631d3fafa94b
ZenGuri <redazengouri@gmail.com>
2023-08-24 18:49:23 -0700

    task 0-print_list.c

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,20 +1,16 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stddef.h>
-
-typedef struct Node
+/* Define your structure here */
+typedef struct list_s
 {
     char *str;
     unsigned int len;
-    struct Node *next;
-} Node;
+    struct list_s *next;
+} list_t;
 
-size_t print_list(const Node *h);
-size_t list_len(const Node *h);
-Node *add_node(Node **head, const char *str);
-Node *add_node_end(Node **head, const char *str);
-void free_list(Node *head);
+/* Function prototype for print_list */
+size_t print_list(const list_t *h);
 
-#endif
+#endif /* LISTS_H */
 
